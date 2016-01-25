fba9f0c3cf8e45d4877c864c9e2b6c7356cb65ad
PockOokbee <santi@ookbee.com>
2016-01-25 16:45:28 +0700

    add new

---
+++ TestGit/Controllers/PockController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestGit.Controllers
+{
+    public class PockController : Controller
+    {
+        // GET: Pock
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
